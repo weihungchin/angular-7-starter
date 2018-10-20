9c286eb8af3ce958fe270c998d98fca2c9753035
Chin Wei Hung <weihung.chin@sicmsb.com>
2018-10-20 14:16:59 +0800

    add new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FoodComponent } from './food/food.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FoodComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
